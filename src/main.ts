3ab19f69d3b63ba2946072c72865300e125243db
Saian Ford <saianford@MacBook-Pro-de-Saian.local>
2023-04-30 22:41:29 -0700

    Process Env fix on main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
