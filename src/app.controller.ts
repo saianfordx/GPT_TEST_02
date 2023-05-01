0d839ba1acbd1c89a8c84c291becce33328165ea
Saian Ford <saianford@MacBook-Pro-de-Saian.local>
2023-05-01 10:57:37 -0700

    GPTTurbo and code cleanup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,24 +4,10 @@ import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly configService: ConfigService,
-  ) {}
-
-  @Get('env-test')
-  getEnvTest(): string {
-    const apiKey = this.configService.get<string>('OPENAI_API_KEY');
-    return apiKey;
-  }
-
-  @Get('test')
-  getestt(): string {
-    return 'Server Works';
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Post('ask')
   async chatGPT(@Body('prompt') prompt: string): Promise<string> {
-    return await this.appService.chatGPT(prompt);
+    return await this.appService.chatGPTTurbo(prompt);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -32,4 +32,28 @@ export class AppService {
       throw new BadRequestException(error.message);
     }
   }
+
+  async chatGPTTurbo(prompt: string): Promise<string> {
+    try {
+      const apiKey = this.configService.get<string>('OPENAI_API_KEY');
+      const response = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [{ role: 'user', content: prompt }],
+          temperature: 0.7,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+          },
+        },
+      );
+
+      return response.data.choices[0].message.content;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
 }
