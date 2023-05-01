bd652e3de8a28ccdf9674d9870963cde1fc8cadb
Saian Ford <saianford@MacBook-Pro-de-Saian.local>
2023-04-30 22:16:36 -0700

    TryCatch

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 
@@ -7,25 +7,29 @@ export class AppService {
   constructor(private configService: ConfigService) {}
 
   async chatGPT(prompt: string): Promise<string> {
-    const apiKey = this.configService.get<string>('OPENAI_API_KEY');
+    try {
+      const apiKey = this.configService.get<string>('OPENAI_API_KEY');
 
-    const response = await axios.post(
-      'https://api.openai.com/v1/engines/davinci-codex/completions',
-      {
-        prompt,
-        max_tokens: 50,
-        n: 1,
-        stop: null,
-        temperature: 0.5,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${apiKey}`,
+      const response = await axios.post(
+        'https://api.openai.com/v1/engines/davinci-codex/completions',
+        {
+          prompt,
+          max_tokens: 50,
+          n: 1,
+          stop: null,
+          temperature: 0.5,
         },
-      },
-    );
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+          },
+        },
+      );
 
-    return response.data.choices[0].text.trim();
+      return response.data.choices[0].text.trim();
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
